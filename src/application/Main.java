d8054a6081377a4b7fb4a0e7291d84efbe7f08e3
Wendel Duarte <wendel_duarte@outlook.com>
2020-07-16 23:04:39 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,7 +21,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Test");
 			primaryStage.show();
@@ -28,6 +30,9 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
